dfe78fd8c22384731461d43cb5474df5fc856ffb
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-10 18:31:31 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo $pwd
 
 ART=$(wget -O - --no-check-certificate https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/ | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
 
