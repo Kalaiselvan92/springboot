00fa60c2adff0e6276096e03a4d1297cb81b8482
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-10 18:07:29 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/runner
 ART= $(curl https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/ | jq -r ".children[].uri" | tr -d '/' | tail -1)
 wget -N https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/wss/wss/0.0.1-SNAPSHOT/$ART && mv $ART spring.jar
 
