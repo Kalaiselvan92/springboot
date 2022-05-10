26b88e353ef18c29c27ad45b1c7b071d7b4fef7d
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-10 18:23:11 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-cd /home/runner
+
 ART=$(wget -O - --no-check-certificate https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/ | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
-wget -N https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/wss/wss/0.0.1-SNAPSHOT/$ART
-mv /home/runner/$ART /home/runner/spring.jar
-java -jar spring.jar &
+
+cd /home/runner; wget -N https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/wss/wss/0.0.1-SNAPSHOT/$ART; mv /home/runner/$ART /home/runner/spring.jar
+
+java -jar /home/runner/spring.jar &
 
 echo "http://localhost:8080/employee"
 
