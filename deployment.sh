2ca82610cf8e1266b47f2517a3e252379ae60d1f
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-10 18:34:45 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -7,8 +7,4 @@ cd /home/runner
 
 wget -O /home/runner/spring.jar  https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/wss/wss/0.0.1-SNAPSHOT/$ART
 
-java -jar /home/runner/spring.jar &
-
-echo "http://localhost:8080/employee"
-
-echo $(wget --spider -S "http://localhost:8080/employee" 2>&1 | grep -F HTTP)
+java -jar /home/runner/spring.jar 
