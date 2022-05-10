534abd46cf77ea039a24d06aa2ff701abd92632d
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-10 18:56:43 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -3,7 +3,7 @@ echo $pwd
 
 ART=$(wget -O - --no-check-certificate https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/ | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
 
-cd /home/runner; wget -O /home/runner/spring.jar  https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/wss/wss/0.0.1-SNAPSHOT/$ART; ls -lrt
+cd /home/runner; wget -O /home/runner/spring.jar  https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/wss/wss/0.0.1-SNAPSHOT/$ART; ls -lrt; chmod 777 spring.jar
 
 
 java -jar /home/runner/spring.jar 
