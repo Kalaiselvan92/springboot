17fd1d153430d0fd3345ec7fb16ce2f4545aa063
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-11 12:32:27 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -6,6 +6,6 @@ ART=$(curl https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javain
 cd /home/runner; wget --user admin --password Password@123  https://ksdemo.jfrog.io/artifactory/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/$ART; ls -lrt; 
 
 
-java -jar /home/runner/$ART &
+nohup java -jar /home/runner/$ART &
 
 echo $(wget --spider -S "http://localhost:8080/employee" 2>&1 | grep -F HTTP)
