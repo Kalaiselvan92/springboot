310c01b13df02693897672daede188ff20e96b09
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-11 12:37:15 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -8,4 +8,4 @@ cd /home/runner; wget --user admin --password Password@123  https://ksdemo.jfrog
 
 nohup java -jar /home/runner/$ART > log.log 2>&1 & 
 
-echo $(wget --spider -S "http://localhost:8080/employee" 2>&1 | grep -F HTTP)
+echo "$wget --spider -S "http://localhost:8080/employee" 2>&1 | grep -F HTTP"
