673df39d246067ffadc9b3db20c48f7dcfe71ca8
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-11 14:39:42 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#PID= `sudo netstat -ltnp | grep java | awk '{print $NF}' | sed "s/\// /g" | awk '{ print $1 }'`
-#kill -9 $PID
-
 
 ART=$(curl https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/ | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
 
@@ -12,4 +9,4 @@ cd /home/ec2-user; rm -rf *.jar; wget --user admin --password Password@123  http
 nohup java -jar /home/ec2-user/$ART
 
 
-##wget -O - --no-check-certificate  http://localhost:8080/employee > /dev/null 2>  /home/ec2-user/response.txt; cat /home/ec2-user/response.txt | grep -F HTTP
+
