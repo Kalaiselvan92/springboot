93bae25c877cb34b6925e3f76c6008718df76967
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-11 13:00:54 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -3,11 +3,11 @@
 
 ART=$(curl https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/ | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
 
-cd /home/runner; wget --user admin --password Password@123  https://ksdemo.jfrog.io/artifactory/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/$ART;
+cd /home/ec2-user; rm -rf *.jar; wget --user admin --password Password@123  https://ksdemo.jfrog.io/artifactory/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/$ART;
 
 
 nohup java -jar /home/runner/$ART > log.log 2>&1 & 
 
-wget -O /dev/null http://localhost:8080/employee 2> /home/runner/response.txt
+wget -O /dev/null http://localhost:8080/employee 2> /home/ec2-user/response.txt
 ls -lrt
-cat /home/runner/response.txt
+cat /home/ec2-user/response.txt | grep "200 OK"
