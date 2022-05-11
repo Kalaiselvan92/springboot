ed779c67f532aba22502385c1877364cf0cc01f0
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-11 13:18:41 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -6,9 +6,4 @@ ART=$(curl https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javain
 cd /home/ec2-user; rm -rf *.jar; wget --user admin --password Password@123  https://ksdemo.jfrog.io/artifactory/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/$ART;
 
 
-nohup java -jar /home/ec2-user/$ART > log.log 2>&1 & 
-
-wget -O - --no-check-certificate  http://localhost:8080/employee > /dev/null 2>  /home/ec2-user/response.txt
-
-
-cat /home/ec2-user/response.txt | grep -F HTTP
+nohup java -jar /home/ec2-user/$ART > log.log 2>&1 &; wget -O - --no-check-certificate  http://localhost:8080/employee > /dev/null 2>  /home/ec2-user/response.txt; cat /home/ec2-user/response.txt | grep -F HTTP
