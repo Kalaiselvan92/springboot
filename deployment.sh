315974a7689abea010d26ef23ed8841af662a574
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-11 11:54:09 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-echo $pwd
-
-ART=$(wget -O - --no-check-certificate https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/ | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
-
-cd /home/runner; wget -O /home/runner/spring.jar  https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/$ART; ls -lrt; chmod +x spring.jar
 
 
-java -jar /home/runner/spring.jar 
+ART=$(curl https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/ | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
+
+cd /home/runner; wget --user admin --password Password@123  https://ksdemo.jfrog.io/artifactory/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/$ART; ls -lrt; 
+
+
+java -jar /home/runner/spring.jar &
+
+echo $(wget --spider -S "http://localhost:8080/employee" 2>&1 | grep -F HTTP)
