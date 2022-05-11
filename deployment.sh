a18afcf7e3b93f37bbfddec9065ddce372a25fdc
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-11 14:26:57 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PID= `sudo netstat -ltnp | grep java | awk '{print $NF}' | sed "s/\// /g" | awk '{ print $1 }'`
-kill -9 $PID
+#PID= `sudo netstat -ltnp | grep java | awk '{print $NF}' | sed "s/\// /g" | awk '{ print $1 }'`
+#kill -9 $PID
 
 
 ART=$(curl https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/ | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
